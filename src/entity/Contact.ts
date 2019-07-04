05094664860da67fddd6de272b03e236d501a842
charlesjjj <chalsa4u@gmail.com>
2019-07-04 13:49:16 +0530

    tables changed

--- src/entity/Contact.ts
+++ src/entity/Contact.ts
@@ -1,10 +1,48 @@
-import {Entity, PrimaryGeneratedColumn, Column, BaseEntity,ManyToOne,JoinColumn} from "typeorm";
-import { Users } from './User';
+// import {Entity, PrimaryGeneratedColumn, Column, BaseEntity,ManyToOne,JoinColumn} from "typeorm";
+// import { Users } from './User';
 
-@Entity("contacts")
-export class Contacts extends BaseEntity{
+// @Entity("contacts")
+// export class Contacts extends BaseEntity{
 
-    @PrimaryGeneratedColumn()
+//     @PrimaryGeneratedColumn()
+//     id: number;
+
+//     @Column("text")
+//     name: string;
+
+//     @Column("text")
+//     village: string;
+
+//     @Column("text")
+//     district: string;
+
+//     @Column("text")
+//     crop: string;
+
+//     @Column("text")
+//     contact_no: number;
+
+//     @Column("text")
+//     rca_id: number;
+
+
+
+//     // @ManyToOne(()=>Users,user=>user.contacts)
+//     // @JoinColumn({name:"rca_id"})
+//     // user:Users
+
+//     @ManyToOne(() => Users)
+//     @JoinColumn({name: 'rca_id', referencedColumnName: 'id'})
+//     user: Users;
+    
+
+// }
+import { Column, Entity,PrimaryGeneratedColumn,BaseEntity, ManyToOne,JoinColumn} from 'typeorm';
+import User from '../entity/User';
+ 
+@Entity()
+export class Contact extends BaseEntity {
+     @PrimaryGeneratedColumn()
     id: number;
 
     @Column("text")
@@ -24,14 +62,11 @@ export class Contacts extends BaseEntity{
 
     @Column("text")
     rca_id: number;
-
-
-
-    @ManyToOne(()=>Users,user=>user.contacts)
-    @JoinColumn({name:"rca_id"})
-    owner:Users
-
-    
-
+ 
+  @ManyToOne(() => User, user=> user.contact)
+  @JoinColumn({ name: "rca_id" })
+   user: User;
+  
 }
  
+export default Contact;
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,11 +1,42 @@
-import {Entity, PrimaryGeneratedColumn, Column, BaseEntity,OneToMany} from "typeorm";
-import { Contacts } from './Contact';
+// import {Entity, PrimaryGeneratedColumn, Column, BaseEntity,OneToMany} from "typeorm";
+// import { Contacts } from './Contact';
 
+// @Entity()
+// export class Users extends BaseEntity{
+
+//     @PrimaryGeneratedColumn()
+//     id: number;
+
+//     @Column("text")
+//     first_name: string;
+
+//     @Column("text")
+//     last_name: string;
+
+//     @Column("text")
+//     phone: number;
+ 
+//     @Column("text")
+//     remember_token: string;
+
+//     @Column("text")
+//     username: string;
+
+//     @Column("text")
+//     password: string;
+
+//     @OneToMany(()=>Contacts,contact=>contact.user)
+//     contacts:Contacts[];
+
+// }
+import { Column, Entity, PrimaryGeneratedColumn ,BaseEntity, OneToMany} from 'typeorm';
+import Contact from '../entity/Contact';
+//import Post from '../post/post.entity';
+ 
 @Entity()
-export class Users extends BaseEntity{
-
-    @PrimaryGeneratedColumn()
-    id: number;
+export class User extends BaseEntity{
+      @PrimaryGeneratedColumn()
+      id: number;
 
     @Column("text")
     first_name: string;
@@ -24,8 +55,10 @@ export class Users extends BaseEntity{
 
     @Column("text")
     password: string;
-
-    @OneToMany(()=>Contacts,contact=>contact.owner)
-    contacts:Contacts[];
-
+ 
+    @OneToMany(() => Contact, contact=> contact.user)
+   contact: Contact[];
+  
 }
+ 
+export default User;
